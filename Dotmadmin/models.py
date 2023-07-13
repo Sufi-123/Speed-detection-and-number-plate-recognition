eb46b768f917c8b8744f23749c36bffda36fabb9
Your Name <you@example.com>
2023-07-13 15:01:07 +0545

    models

--- Dotmadmin/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Station(models.Model):
-    # stationID= models.ForeignKey('Station', on_delete=models.CASCADE)
-    IDs= models.IntegerField()
-    location = models.CharField(max_length=80)
-    areacode = models.PositiveIntegerField()
-    mac_address = models.CharField(max_length=17)
